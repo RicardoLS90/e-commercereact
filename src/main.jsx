48ed8fe0502a858ea061d6a93695652d96511d96
RicardoAcademlo <rleons40@gmail.com>
2022-12-01 15:50:37 -0500

    actualizado

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
 import 'bootswatch/dist/sandstone/bootstrap.min.css';
-<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
